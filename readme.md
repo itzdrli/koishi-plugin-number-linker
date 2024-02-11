a08c78fe53328d5693481360d4177b2d3d95644e
Itz_Dr_Li <liletian1231@outlook.com>
2024-02-11 18:38:45 +0000

    hi

--- readme.md
+++ readme.md
@@ -1,5 +1,20 @@
-# koishi-plugin-number-linker
+# number-linker
 
-[![npm](https://img.shields.io/npm/v/koishi-plugin-number-linker?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-number-linker)
+[![Github](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/itzdtech/koishi-plugin-number-linker) [![npm](https://img.shields.io/npm/v/koishi-plugin-number-linker?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-number-linker)
 
-Fast convert numbers to link
+快速将数字转换为对应的链接
+
+比如这样:
+![J1UOyJI.md.png](https://iili.io/J1UOyJI.md.png)
+或者这样:
+![J1UOLxa.md.png](https://iili.io/J1UOLxa.md.png)
+
+## 安装配置
+ - 插件市场搜索 `number-linker` 并安装
+ - 在配置页面，为 `enabledGuilds` 添加想要启用的群号
+ - 启用插件即可
+
+## 想要新功能？
+pr welcome [![Github](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/itzdtech/koishi-plugin-number-linker)
+
+Copyright © 2024 [ItzDTech](https://dev.itzdrli.com)
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,30 @@ import { Context, Schema } from 'koishi'
 
 export const name = 'number-linker'
 
-export interface Config {}
+export interface Config {
+  enabledGuilds: Array<string>,
+  frontLink: string,
+  backLink: string,
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  enabledGuilds: Schema.array(String).role('table').description('启用的群组').required(),
+  frontLink: Schema.string().default("https://dev.itzdrli.com/topic/").description("链接的前半部分(数字前的内容)"),
+  backLink: Schema.string().default("/").description("链接的后半部分")
+})
 
-export function apply(ctx: Context) {
-  // write your plugin here
+function isNumericAndLessThanFourDigits(input: string): boolean { //Thank you ChatGPT
+  const regex = /^\d{1,4}$/;
+  return regex.test(input);
+}
+
+export function apply(ctx: Context, config: Config) {
+  ctx.on('message-created', (session) => {
+    if (!isNumericAndLessThanFourDigits(session.content)) return
+    if (!config.enabledGuilds.includes(session.guildId)) return
+
+    const response = config.frontLink + session.content + config.backLink
+    session.send(response)
+    return
+  })
 }
